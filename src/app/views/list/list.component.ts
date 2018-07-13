1342531bb9f7cbf8ba92181e8fe71af3108c76d7
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-07-13 15:46:51 -0300

    initial commit

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -6,17 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  selectedPkm = null;
   pokemonList = [
     { name: 'Bulbasaur', number: 1 },
     { name: 'Charmander', number: 4 },
     { name: 'Squirtle', number: 7 },
     { name: 'Pikachu', number: 25 }
   ]
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  selectPokemon(pkm){
+    this.selectedPkm = pkm;
   }
 
 }
