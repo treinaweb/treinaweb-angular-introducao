f5582974a527f91058bf902423fd818b70b8a59d
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-07-13 15:30:01 -0300

    initial commit

---
+++ src/app/meu-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-componente-component',
+    template: `<div>Meu Componente</div>`
+})
+export class MeuComponenteComponent{
+
+}
