3289b0755c1f1814b363f00360a16c388a4d07e3
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-07-13 15:46:22 -0300

    initial commit

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  pokemonList = [
+    { name: 'Bulbasaur', number: 1 },
+    { name: 'Charmander', number: 4 },
+    { name: 'Squirtle', number: 7 },
+    { name: 'Pikachu', number: 25 }
+  ]
+
   constructor() { }
 
   ngOnInit() {
